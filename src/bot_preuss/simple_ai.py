d536defa8d7053dd63edc7bf47ebbc6afcc77cd0
Asbjørn B. Preuss <afbpr@dtu.dk>
2025-08-26 20:01:26 +0200

    Changes to bot

--- src/bot_preuss/simple_ai.py
+++ src/bot_preuss/simple_ai.py
@@ -20,20 +20,45 @@ def ship_ai(ship, info, game_map):
     """
     Function to control ships.
     """
+    keep_sailing = False
     if not ship.stopped:
+        #print("Ship not stopped")
         if ship.stuck:
-            if ship.get_distance(ship.owner.x, ship.owner.y) > 20:
-                ship.convert_to_base()
+            print("Ship stuck")
+            for base in info["bases"]:
+                print("Checking Base", base.x,base.y)
+                if ship.get_distance(base.x,base.y)<60:
+                    print("Too close to home")
+                    keep_sailing = True
+                    break
+        
+                    
+            
+            
+            #reverse and change angle
+            if keep_sailing:
+                ship.set_heading(((ship.heading+180)%360 + np.random.normal(loc=0,scale=15))%360)
             else:
-                ship.set_heading(np.random.random() * 360.0)
+                ship.convert_to_base()
+      
 
 
 def jet_ai(jet, info, game_map):
     """
     Function to control jets.
     """
+    bases = info["enemy_bases"]
+    for ind in range(len(bases)):
+        if ind == 0:
+            dist = jet.get_distance(bases[ind].x,bases[ind].y)
+            target = 0
+        else:
+            if jet.get_distance(bases[ind].x,bases[ind].y) < dist:
+                dist = jet.get_distance(bases[ind].x,bases[ind].y)
+                target = ind
+
     if "target" in info:
-        jet.goto(*info["target"])
+        jet.goto(bases[target].x, bases[target].y)
 
 
 class PlayerAi:
@@ -43,9 +68,22 @@ class PlayerAi:
 
     def __init__(self, name):
         self.team = name
-        self.build_queue = helpers.BuildQueue(
-            ["mine", "tank", "ship", "jet"], cycle=True
+        self.build_queue_start = helpers.BuildQueue(
+            ["mine", "tank", "ship"],  cycle=True
         )
+        self.build_queue_settler = helpers.BuildQueue(
+            ["tank", "ship"],  cycle=True
+        )
+        self.build_queue_warrior = helpers.BuildQueue(
+            ["jet", "mine"],  cycle=True
+        )
+        self.build_queue_sailor = helpers.BuildQueue(
+            ["ship"],  cycle=True
+        )
+        self.bases_status = {}
+        self.enemies = []
+        
+
 
     def run(self, t: float, dt: float, info: dict, game_map: np.ndarray):
         """
@@ -54,25 +92,53 @@ class PlayerAi:
 
         # Get information about my team
         myinfo = info[self.team]
+        
 
         # Iterate through all my bases and process build queue
         for base in myinfo["bases"]:
+            if (len(myinfo["bases"]) < 2) and (np.random.random()<.25):
+                obj = self.build_queue_sailor(base)
+                continue
+
+            # Create status for new objects in dict
+            self.bases_status[base.uid] =  self.bases_status.get(base.uid, "start")
+
             # Calling the build_queue will return the object that was built by the base.
             # It will return None if the base did not have enough resources to build.
-            obj = self.build_queue(base)
+            if self.bases_status[base.uid] == "start":
+                obj = self.build_queue_start(base)
+                if obj is not None:
+                    if obj.kind == "ship":
+                        print("Time to explore")
+                        self.bases_status[base.uid] = "settler"
+
+            elif self.bases_status[base.uid] == "settler":
+                obj = self.build_queue_settler(base)
+                if np.random.random() < (t)/400:
+                    print("Time for war")
+                    self.bases_status[base.uid] = "warrior"
+
+            elif self.bases_status[base.uid] == "warrior":
+                if np.random.random() < 0.1:
+                    obj = self.build_queue_sailor(base)
+                else:
+                    obj = self.build_queue_warrior(base)
+                    
+
 
         # Try to find an enemy target
         # If there are multiple teams in the info, find the first team that is not mine
         if len(info) > 1:
+            self.enemies = []
+            mindist = 1e9
             for name in info:
                 if name != self.team:
                     # Target only bases
                     if "bases" in info[name]:
                         # Simply target the first base
-                        t = info[name]["bases"][0]
-                        myinfo["target"] = [t.x, t.y]
-                        break
-
+                        self.enemies += info[name]["bases"]
+                
+        myinfo["enemy_bases"] = self.enemies
         # Control all my vehicles
         helpers.control_vehicles(
             info=myinfo, game_map=game_map, tank=tank_ai, ship=ship_ai, jet=jet_ai
